f2cd2fab8487399c16b0a5d856ab3988db4ab5d2
vladimirtischler <tischler.vladimir@gmai.com>
2019-10-10 08:29:33 +0200

    openlab

---
+++ open-lab-01.3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace open_lab_01._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int b = int.Parse(a);
+            int c = ++b;
+            Console.WriteLine(c);
+        }
+    }
+}
